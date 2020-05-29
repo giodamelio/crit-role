d589b3b0775b24d46753d68944ff6f471ef5caaf
Gio d'Amelio <giodamelio@gmail.com>
2020-05-28 22:19:02 -0700

    Simple watch page with series and episode url segments

---
+++ pages/watch/_series/_episode.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Viewing Episode: {{ series }}/{{ episode }}</h1>
+</template>
+
+<script lang="ts">
+export default {
+  validate({ params }: { params: any }) {
+    console.log(params);
+    return true;
+  },
+  data() {
+    return {
+      series: this.$route.params.series,
+      episode: this.$route.params.episode
+    };
+  }
+};
+</script>
+
+<style></style>
