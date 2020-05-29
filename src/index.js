fa8ebd51e3f139dc9dd84aa3ca3f4375cbefc3cf
Gio d'Amelio <giodamelio@gmail.com>
2020-05-29 15:27:17 -0700

    Setup Koa router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
+router.get('/', async ctx => {
   ctx.body = 'Hello World';
 });
+
+router.post('/ping', async ctx => {
+  ctx.body = { response: 'pong' };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3141);
 console.log('App running on port: 3141');
