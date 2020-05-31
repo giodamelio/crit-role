c0e14df2a5f9991da8a5f41b7e1e1483424f5ee5
Gio d'Amelio <giodamelio@gmail.com>
2020-05-30 22:34:45 -0700

    Setup Jest and unit test the middleware

---
+++ test/unit/middleware/error.spec.ts
@@ -0,0 +1,45 @@
+import { createMockContext } from '@shopify/jest-koa-mocks';
+
+// Test subject
+import error from '../../../src/middleware/error';
+
+describe('Middleware', () => {
+  describe('error', () => {
+    it('Should catch an error', async () => {
+      expect.assertions(4);
+
+      // Setup mocks for context and children middleware
+      const mockContext = createMockContext();
+      const errorEventHandler = jest.fn();
+      mockContext.app.on('error', errorEventHandler);
+      const failingMiddleware = jest.fn(async () => {
+        throw new Error('Hello World');
+      });
+
+      await error(mockContext, failingMiddleware);
+
+      expect(failingMiddleware).toHaveBeenCalled();
+      expect(errorEventHandler).toHaveBeenCalled();
+      expect(mockContext.status).toBe(500);
+      expect(mockContext.body).toBe('Internal Server Error');
+    });
+
+    it('Should pass silently with no errors', async () => {
+      expect.assertions(3);
+
+      // Setup mocks for context and children middleware
+      const mockContext = createMockContext();
+      const errorEventHandler = jest.fn();
+      mockContext.app.on('error', errorEventHandler);
+      const failingMiddleware = jest.fn(async () => {
+        // Do nothing
+      });
+
+      await error(mockContext, failingMiddleware);
+
+      expect(failingMiddleware).toHaveBeenCalled();
+      expect(errorEventHandler).not.toHaveBeenCalled();
+      expect(mockContext.status).toBe(404);
+    });
+  });
+});
---
+++ test/unit/middleware/nunjucks.spec.ts
@@ -0,0 +1,67 @@
+import { createMockContext } from '@shopify/jest-koa-mocks';
+import nj from 'nunjucks';
+
+// Test subject
+import nunjucks, { asyncRender } from '../../../src/middleware/nunjucks';
+
+describe('Middleware', () => {
+  describe('nunjucks', () => {
+    it('Should render a simple template', async () => {
+      expect.assertions(3);
+
+      // Setup mocks for context and children middleware
+      const mockContext = createMockContext();
+      const noopMiddleware = jest.fn();
+
+      // Run the middleware to add the `render` function to the context
+      const nunjucksMiddleware = nunjucks('test/unit/middleware/views');
+      await nunjucksMiddleware(mockContext, noopMiddleware);
+
+      await mockContext.render('test.njk', { name: 'Roy Choi' });
+
+      expect(noopMiddleware).toHaveBeenCalled();
+      expect(mockContext.type).toBe('text/html');
+      expect(mockContext.body).toBe('Hello Roy Choi!\n');
+    });
+
+    describe('asyncRender()', () => {
+      it('Render a template', async () => {
+        expect.assertions(2);
+
+        const njMock = {
+          render: jest.fn(async (_templatePath, _data, callback) => {
+            callback(null, 'Hello!');
+          }),
+        };
+
+        const rendered = await asyncRender(
+          (njMock as unknown) as nj.Environment,
+          'not_a_real_file.njk',
+          {}
+        );
+
+        expect(rendered).toBe('Hello!');
+        expect(njMock.render).toHaveBeenCalled();
+      });
+
+      it('Error during rendering', async () => {
+        expect.assertions(2);
+
+        const njMock = {
+          render: jest.fn(async (_templatePath, _data, callback) => {
+            callback(new Error('Cannot render'), null);
+          }),
+        };
+
+        const renderPromise = asyncRender(
+          (njMock as unknown) as nj.Environment,
+          'not_a_real_file.njk',
+          {}
+        );
+
+        await expect(renderPromise).rejects.toThrowError('Cannot render');
+        expect(njMock.render).toHaveBeenCalled();
+      });
+    });
+  });
+});
