5a664e92e5d7012e7dcaf2a2f70e198369cc0743
Gio d'Amelio <giodamelio@gmail.com>
2020-06-01 13:58:31 -0700

    Update the logger to have some custom options for the local pretty printer

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,15 @@
 import pino from 'pino';
 
+const localLogFormatOptions = {
+  translateTime: true,
+  messageFormat: '[{step}] {msg}',
+  ignore: 'hostname,pid',
+};
+
 export default pino({
   // Pretty print when not in production
-  prettyPrint: process.env.NODE_ENV !== 'production',
+  prettyPrint:
+    process.env.NODE_ENV !== 'production' ? localLogFormatOptions : false,
 
   // Disable logger in test
   enabled: process.env.NODE_ENV !== 'test',
