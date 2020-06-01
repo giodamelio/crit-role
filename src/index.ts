23c1a13fd8a48f8d2eddc00a84fd936cb15b6384
Gio d'Amelio <giodamelio@gmail.com>
2020-05-31 22:29:40 -0700

    Add a body parser. Make the random numbers more random (also in hex)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { randomBytes } from 'crypto';
+import { promisify } from 'util';
+
 import Koa from 'koa';
 import Router from '@koa/router';
 import helmet from 'koa-helmet';
 import koaPinoLogger from 'koa-pino-logger';
+import bodyparser from 'koa-bodyparser';
 
 import logger from './logger';
 import { error as errorMiddleware, nunjucks } from './middleware';
@@ -17,13 +21,17 @@ router.post('/ping', (ctx: Koa.ParameterizedContext) => {
   ctx.body = { response: 'pong' };
 });
 
+const randomBytesPromise = promisify(randomBytes);
 router.get('/random', async (ctx: Koa.ParameterizedContext) => {
-  await ctx.render('random_number.njk', { number: Math.random() });
+  const len = Number(ctx.request.query.len || 16);
+  const number = (await randomBytesPromise(len)).toString('hex');
+  await ctx.render('random_number.njk', { number });
 });
 
 app.use(errorMiddleware);
 app.use(koaPinoLogger({ logger }));
 app.use(helmet());
+app.use(bodyparser());
 app.use(nunjucks('views'));
 
 app.use(router.routes()).use(router.allowedMethods());
