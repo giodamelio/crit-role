cbd941ab53807494ad394cda97f18e9c9341ec8c
Gio d'Amelio <giodamelio@gmail.com>
2020-06-01 19:44:09 -0700

    Enable ESLint Plugin Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'airbnb-typescript/base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/all',
   ],
+  rules: {
+    // I like almost all of the ESLint Jest plugin's rules. Except these:
+    'jest/no-hooks': 'off',
+    'jest/no-expect-resolves': 'off',
+    'jest/no-restricted-matchers': 'off',
+    'jest/prefer-inline-snapshots': 'off',
+  },
 };
